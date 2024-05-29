7528043b0a99f4f160965e0ee6c74b0e50a9f672
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2024-05-29 20:59:58 +0900

    서버 비주얼 스튜디오 세팅

---
+++ network_final_Server/network_final_Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace network_final_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
